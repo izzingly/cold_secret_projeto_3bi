ec503d8a839d43e75b41fe06ae328394996fbd33
Allan Rafael <allanrafael0810@gmail.com>
2025-09-15 09:05:32 -0300

    menu feito 09:05
    allan

--- cold_secret_unity_project/Assets/scripts/MenuPrincipalManager.cs
+++ cold_secret_unity_project/Assets/scripts/MenuPrincipalManager.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuPrincipalManager : MonoBehaviour
 {
     [SerializeField] private string nomeDoLevelDeJogo;
     [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelOpcoes;
-
+    [SerializeField] private string NomeDeMenu;
     [SerializeField] private GameObject PainelTutorial;
+    [SerializeField] private GameObject painelDeConfirmação;
 
     public void Jogar()
     {
         SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(NomeDeMenu);
     }
     
     public void AbrirOpcoes()
@@ -40,10 +49,27 @@ public class MenuPrincipalManager : MonoBehaviour
         painelMenuInicial.SetActive(true);
     }
 
+     public void AbrirConfirmação()
+    {
+        painelMenuInicial.SetActive(false);
+        painelDeConfirmação.SetActive(true);
+    }
+
+    public void FecharConfirmação()
+    {
+        painelDeConfirmação.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+
     public void SairJogo()
     {
-        Debug.Log("Sair do Jogo");
+        Debug.Log("Saiu do jogo");
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
         Application.Quit();
+        #endif
+        
     }
 
 }
