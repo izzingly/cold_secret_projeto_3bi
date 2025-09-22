038cb5d251b69f688a2a4072c9a8015755cd376c
Daniel C. Schaefer <danielschaefer2805@gmail.com>
2025-09-22 12:00:02 -0300

    11:52 22/09
    interact scripts

---
+++ cold_secret_unity_project/Assets/scripts/PlayerInteract.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public float rayDistance = 2f;
+    public float rotateSpeed = 200;
+    public Transform objectViewer;
+    public Transform objectReader;
+    public Transform objectUse;
+    public UnityEvent OnView;
+    private Camera myCam;
+    private bool isViewing;
+    private Interactables currentInteractable;
+    public bool userInteract = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInteractables();
+    }
+     public void CheckInteractables()
+    {
+        if (isViewing)
+        {
+            // if (currentInteractable.item.grabbable && Input.GetMouseButtonDown(0))
+            if (currentInteractable.item.grabbable && userInteract)
+            {
+                userInteract = false;
+                //RotateObject  
+            }
+        }
+        RaycastHit hit;
+        Vector3 rayOrigin = myCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.5f));
+        if (Physics.Raycast(rayOrigin, myCam.transform.forward, out hit))
+        {
+            Interactables interactable = hit.collider.GetComponent<Interactables>();
+            Interactables interacte = hit.collider.GetComponent<Interactables>();
+
+            if (userInteract)
+                {
+                    OnView.Invoke();
+                    currentInteractable = interactable;
+                    isViewing = true;
+                }
+            else
+            {
+                // Saiu de cima de um objeto
+                UIManager.instance.SetInteractCursor(false);
+            }
+        }
+    }
+}
+    
--- cold_secret_unity_project/Assets/scripts/UIManager.cs
+++ cold_secret_unity_project/Assets/scripts/UIManager.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
+    public GameObject interactCursor;
+    public GameObject cursor;
+    private void Awake()
+    {
+        instance = this;
+    }
 
-
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +23,13 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void SetInteractCursor(bool state)
+    {
+        interactCursor.SetActive(state);
+    }
+    public void SetCursor(bool state)
+    {
+        cursor.SetActive(state);
     }
 }
