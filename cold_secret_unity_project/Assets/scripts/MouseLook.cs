81e0a38fb94566eb87b8f1eefc2f6848d8e05658
Allan Rafael <allanrafael0810@gmail.com>
2025-10-03 19:59:45 -0300

    interface pra mobile 19:59 allan

--- cold_secret_unity_project/Assets/scripts/MouseLook.cs
+++ cold_secret_unity_project/Assets/scripts/MouseLook.cs
@@ -15,7 +15,6 @@ public class MouseLook : MonoBehaviour
     void Start()
     {
         //Cursor.lockState = CursorLockMode.Locked;
-        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
